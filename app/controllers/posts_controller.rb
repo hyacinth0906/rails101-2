33d1ba776d8db593ca9e49d7071a637d49b93c9c
hyacinth0906 <doris_0906@126.com>
2017-01-06 13:17:04 +0800

    implement groups#update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
-  before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:new, :create, :edit, :destroy]
+  before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -18,12 +19,30 @@ class PostsController < ApplicationController
     else
       render :new
     end
+
+    def edit
+    end
+
+    def destroy
+      @post.destroy
+      flash[:alert] = "Post deleted"
+      redirect_to root_path
+    end
+
 end
 
 private
 
 def post_params
   params.require(:post).permit(:content)
+end
+
+def find_post_and_check_permission
+  @post = Post.find(params[:group_id])
+
+  if current_user != @post.user
+    redirect_to root_path, alert: "You have no permission."
+  end
 end
 
 end
